c91b7fe715b72011d43b2022d2f71ed654aee834
kimchanghyun0604 <golf06040606@gmail.com>
2022-09-26 15:33:16 +0900

    merge 커밋

--- src/git/Main.java
+++ src/git/Main.java
@@ -1,9 +1,12 @@
 package git;
 
 public class Main {
-	public static void main(String[] args) {
-		String s = "안녕하세요";
+	static void say(String s) {
 		System.out.println(s);
+	}
+
+	public static void main(String[] args) {
+		say("안녕하세요");
 	}
 
 }
